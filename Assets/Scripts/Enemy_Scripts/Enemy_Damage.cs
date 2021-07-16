836fa134d802f253c2e46168c56669d044b2dc13
Riley <Riley@ipartneruniforms.com>
2021-07-16 15:31:01 -0700

    Added damage system so enemy can die

---
+++ Assets/Scripts/Enemy_Scripts/Enemy_Damage.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Damage : MonoBehaviour {
+
+    private float maxHealth = 10.0f;
+    public float health = 1.0f;
+
+    private float damageTime = 0.0f;
+    private float maxDmgTime = 0.75f;
+    
+    
+    void OnTriggerEnter2D(Collider2D collider) {
+
+        if(collider.tag == "PlayerAttack") {
+            if(damageTime <= 0.0f) {
+                health -= collider.gameObject.GetComponent<Damage_Storage>().damage;
+
+                damageTime = maxDmgTime;
+
+                if(health <= 0.0f) {
+                    onDeath();
+                }
+            }
+        }
+
+    }
+
+
+    void onDeath() {
+        this.gameObject.SetActive(false);
+    }
+
+
+    void Start() {
+        health = maxHealth;
+    }
+
+
+    void Update() {
+
+        if(damageTime > 0.0f) {
+            damageTime -= Time.deltaTime;
+        }
+
+        if(damageTime <= 0.0f) {
+            damageTime = 0.0f;
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Storage/Damage_Storage.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage_Storage : MonoBehaviour {
+	
+	public float damage;
+	
+}
