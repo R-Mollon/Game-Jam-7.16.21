da8ea3c413c171f2a2a0c7823c0072d3a9dff974
Riley <Riley@ipartneruniforms.com>
2021-07-16 18:44:01 -0700

    Added player model

--- Assets/Scripts/Player_Scripts/MovementScript.cs
+++ Assets/Scripts/Player_Scripts/MovementScript.cs
@@ -4,34 +4,43 @@ using UnityEngine;
 
 public class MovementScript : MonoBehaviour {
    
-    private float playerSpeed = 2.0f;
+    private float playerSpeed = 8.0f;
+    private int playerDirection = 0;
 
     private Rigidbody2D rigidBody;
     private Camera camera;
+    private SpriteRenderer playerSprite;
+    private Sprite[] playerSpriteList;
 
     public bool cameraLocked = true;
 
     void Start() {
         rigidBody = GetComponent<Rigidbody2D>();
         camera = GameObject.FindGameObjectsWithTag("MainCamera")[0].GetComponent<Camera>();
+        playerSprite = GetComponent<SpriteRenderer>();
+        playerSpriteList = Resources.LoadAll<Sprite>("Sprites/Player");
     }
 
 
-    void FixedUpdate() {
+    void Update() {
 
         Vector2 direction = new Vector2(0, 0);
 
         if(Input.GetKey(KeyCode.W)) {
             direction.y = 1;
+            playerDirection = 2;
         }
         if(Input.GetKey(KeyCode.A)) {
             direction.x = -1;
+            playerDirection = 4;
         }
         if(Input.GetKey(KeyCode.S)) {
             direction.y = -1;
+            playerDirection = 0;
         }
         if(Input.GetKey(KeyCode.D)) {
             direction.x = 1;
+            playerDirection = 1;
         }
 
         rigidBody.MovePosition(rigidBody.position + (direction * playerSpeed * Time.deltaTime));
@@ -40,6 +49,8 @@ public class MovementScript : MonoBehaviour {
         if(cameraLocked) {
             camera.transform.position = new Vector3(rigidBody.position.x, rigidBody.position.y, -10);
         }
+
+        playerSprite.sprite = playerSpriteList[playerDirection];
 
     }
 
