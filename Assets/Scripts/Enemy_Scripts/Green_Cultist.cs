ad8a0fd4fac977e394732faed94fd577e0a55abf
Riley <Riley@ipartneruniforms.com>
2021-07-22 22:54:59 -0700

    Added green cultist enemy

---
+++ Assets/Scripts/Enemy_Scripts/Green_Cultist.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Green_Cultist : MonoBehaviour {
+
+    public float health = 1f;
+    public float maxHealth = 15f;
+
+    private float dmgCool = 0.0f;
+    private float maxDmgCool = 0.2f;
+    
+    private LineRenderer lineRenderer;
+    private SpriteRenderer spriteRenderer;
+
+    void Start() {
+        lineRenderer = GetComponent<LineRenderer>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        health = maxHealth;
+
+        StartCoroutine("Attack");
+    }
+
+
+    void Update() {
+        if(dmgCool > 0.0f) {
+            dmgCool -= Time.deltaTime;
+
+            spriteRenderer.color = new Color(1, (maxDmgCool - dmgCool) * 5, (maxDmgCool - dmgCool) * 5, 1);
+        }
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "PlayerAttack" && dmgCool <= 0.0f) {
+            health -= other.GetComponent<Damage_Storage>().damage;
+            dmgCool = maxDmgCool;
+
+            if(health <= 0) {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+
+    IEnumerator Attack() {
+
+        float cooldown = Random.Range(0, 4);
+        float maxCooldown = 4.0f;
+
+        while(true) {
+            cooldown -= Time.deltaTime;
+
+            if(cooldown <= 0.0f) {
+
+                float timer = 0.0f;
+                cooldown = maxCooldown;
+
+                while(true) {
+                    lineRenderer.SetPosition(0, transform.position - (Vector3.up * 0.2f));
+                    lineRenderer.SetPosition(1, GameObject.Find("Player").transform.position);
+
+                    timer += Time.deltaTime;
+
+                    if(timer >= 2.0f) {
+                        GameObject projectile = Instantiate(Resources.Load<GameObject>("Prefabs/GreenCultistProjectile"), transform.position, Quaternion.identity);
+                        GreenCultistProjectile script = projectile.GetComponent<GreenCultistProjectile>();
+
+                        lineRenderer.SetPosition(0, transform.position);
+                        lineRenderer.SetPosition(1, transform.position);
+
+                        script.angle = Vector3.Normalize(GameObject.Find("Player").transform.position - transform.position);
+                        script.go();
+
+                        break;
+                    }
+
+                    yield return null;
+                }
+
+            }
+
+            yield return null;
+        }
+
+    }
+
+
+}
---
+++ Assets/Scripts/Projectile_Scripts/GreenCultistProjectile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenCultistProjectile : MonoBehaviour {
+    
+    public Vector3 angle;
+
+    private float speed = 8.0f;
+
+    private bool started = false;
+
+    private float lifetime;
+
+    public void go() {
+        started = true;
+    }
+
+    void Update() {
+        if(!started) {
+            return;
+        }
+
+        transform.position = transform.position + (angle * Time.deltaTime * speed);
+
+        lifetime += Time.deltaTime;
+        if(lifetime > 8.0f) {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Wall") {
+            Destroy(gameObject);
+        }
+    }
+
+}
